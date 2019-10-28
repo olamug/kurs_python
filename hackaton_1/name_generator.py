f0c1a1429dff2b004e169c2281274103f8b09491
olamug <kuberska.aleksandra@gmail.com>
2019-10-28 16:21:18 +0100

    'name_generator_upgrade'

--- hackaton_1/name_generator.py
+++ hackaton_1/name_generator.py
@@ -1,18 +1,22 @@
 import random
 print("This is a name generator. ")
 print()
-list_vowel = "a e i o u y".split(" ")
+list_vowel = "aeio"
+list_vowel2 = "iuy"
 list_consonants = "b c d f g h j k l m n p r s t w x z".split()
 list_2consonants = ['rd', 'st', 'th', 'dr', 'wr', 'mn', 'sp', 'pr']
 
 vowel = random.choice(list_vowel)
+vowel2 = random.choice(list_vowel2)
 consonants2 = random.choice(list_2consonants)
 consonant = random.choice(list_consonants)
 
-a = vowel + consonant + vowel + consonants2
+a = vowel + consonant + vowel2 + consonants2
 b = consonants2 + vowel + consonant
 c = consonant + vowel + consonants2
-d = consonant + vowel + consonants2 + vowel
-name_list = [a, b, c, d]
+d = consonant + vowel + consonants2 + vowel2
+e = vowel + consonant + vowel + consonants2
+f = consonant + vowel + consonant + vowel
+name_list = [a, b, c, d, e, f]
 name = random.choice(name_list).capitalize()
 print(f"Your new name is {name}. \n\n{name} is a praiseworthy name for a warrior.")
